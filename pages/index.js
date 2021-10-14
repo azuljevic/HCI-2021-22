6f8dc7c011f718e7d0a55ff2da65c301f9e66cd3
AndreVidovic <vidaftw@gmail.com>
2021-10-14 10:58:40 +0200

    Add messy code

--- pages/index.js
+++ pages/index.js
@@ -13,10 +13,12 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+
+ Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
+
           Get started by editing{' '}
           <code className={styles.code}>pages/index.js</code>
         </p>
@@ -24,7 +26,9 @@ export default function Home() {
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
             <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
+            <p>Find in-depth information
+             
+               about Next.js features and API.</p>
           </a>
 
           <a href="https://nextjs.org/learn" className={styles.card}>
@@ -36,8 +40,10 @@ export default function Home() {
             href="https://github.com/vercel/next.js/tree/master/examples"
             className={styles.card}
           >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+  <h2>Examples &rarr;</h2>
+            <p>Discover and deploy boilerplate
+             
+               example Next.js projects.</p>
           </a>
 
           <a
